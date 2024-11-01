2a35d5960db42b1d2c2b552aea6668e2881cca99
Jason <extremewinds@gmail.com>
2024-11-01 15:59:12 +0100

    create footer ui

--- apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,8 +1,12 @@
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
   faUser,
   faCartShopping,
-
+  faTruckFast,
+  faYoutube,
+  faTwitter,
+  faFacebook
 ];
